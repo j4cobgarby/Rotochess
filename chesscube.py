6c214a1b9776678aff91ab4cd02ae5a38fb0e26c
Illin Tori <IllinTori@gmail.com>
2022-11-12 15:40:25 +0000

    Split into classess

---
+++ chesscube.py
@@ -0,0 +1,38 @@
+import pyray as pr
+
+class Cubelet:
+    def __init__(self, pos, size, cs):
+        self.dims = [cs, cs, cs]
+        self.pos = [n for n in pos]
+        self.pieces = [None, None, None, None, None, None] # up,down,left,right,front,back
+        self.col = pr.Color(60, 124, 58, 255) \
+            if (self.pos[0] + self.pos[1]*3 + self.pos[2] * 9) % 2 != 0 else pr.WHITE
+        self.cs = cs
+        self.size = size
+
+    def draw(self):
+        pr.draw_cube_v([(n*3) - (self.size*self.cs)/2 + self.cs/2 for n in self.pos], self.dims, self.col)
+
+class ChessCube():
+    def __init__(self,size,cubelet_size):
+        self.size = size
+        self.cubelet_size = cubelet_size
+
+
+        # Initilise Cubelet Array
+        self.cubelets = [[[] for i in range(0,size)]for i in range(0,size)]
+
+        for y in range(0, size):
+            for z in range(0, size):
+                for x in range(0, size):
+                    self.cubelets[y][z].append(Cubelet([x,y,z], size, cubelet_size))
+
+        # Initilise empty piece array
+        self.pieces = [[[None for i in range(size)] for i in range(size)] for i in range (6)]
+
+    def draw(self):
+        for y in range(0, self.size):
+            for z in range(0, self.size):
+                for x in range(0, self.size):
+                    if x in [0, self.size-1] or y in [0, self.size-1] or z in [0, self.size-1]:
+                        self.cubelets[y][z][x].draw()
--- main.py
+++ main.py
@@ -2,33 +2,18 @@
 
 import pyray as pr
 import random
-
-class Cubelet:
-    def __init__(self, pos, size, cs):
-        self.dims = [cs, cs, cs]
-        self.pos = [n for n in pos]
-        self.pieces = [None, None, None, None, None, None] # up,down,left,right,front,back
-        self.col = pr.Color(60, 124, 58, 255) \
-            if (self.pos[0] + self.pos[1]*3 + self.pos[2] * 9) % 2 != 0 else pr.WHITE
-        self.cs = cs
-        self.size = size
-
-    def draw(self):
-        pr.draw_cube_v([(n*3) - (self.size*self.cs)/2 + self.cs/2 for n in self.pos], self.dims, self.col)
+from chesscube import ChessCube
 
 def main():
     size = 8
     cubelet_size = 3
     pr.set_config_flags(pr.ConfigFlags.FLAG_MSAA_4X_HINT)
-    cube = [[[] for i in range(0,size)]for i in range(0,size)]
-
-    for y in range(0, size):
-        for z in range(0, size):
-            for x in range(0, size):
-                cube[y][z].append(Cubelet([x,y,z], size, cubelet_size))
+    pr.set_config_flags(pr.ConfigFlags.FLAG_VSYNC_HINT)
 
     pr.init_window(700, 700, "Rotochess")
     pr.set_target_fps(60)
+
+    world = ChessCube(size,cubelet_size)
 
     camera = pr.Camera3D([18.0, 16.0, 18.0], [0.0, 0.0, 0.0], [0.0, 1.0, 0.0], 45.0, 0)
     camera.projection = pr.CameraProjection.CAMERA_ORTHOGRAPHIC
@@ -41,11 +26,7 @@ def main():
         pr.clear_background(pr.Color(182,191,239,255))
         pr.begin_mode_3d(camera)
         
-        for y in range(0, size):
-            for z in range(0, size):
-                for x in range(0, size):
-                    if x in [0, size-1] or y in [0, size-1] or z in [0, size-1]:
-                        cube[y][z][x].draw()
+        world.draw()
 
         pr.end_mode_3d()
         pr.end_drawing()

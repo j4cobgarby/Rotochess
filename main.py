8d17bbc5c5924f506b1e11efb6f98d2cb8f840af
jacob G <j4cobgarby@gmail.com>
2022-11-12 14:29:53 +0000

    generating cube done

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
+#!/usr/bin/env python
+
 import pyray as pr
 import random
 
 class Cubelet:
-    def __init__(self, pos):
-        self.dims = [3,3,3]
-        self.pos = pos
+    def __init__(self, pos, size, cs):
+        self.dims = [cs, cs, cs]
+        self.pos = [n for n in pos]
         self.pieces = [None, None, None, None, None, None] # up,down,left,right,front,back
-        self.col = random.choice([pr.BLUE, pr.RED, pr.BLACK, pr.MAROON, pr.PURPLE])
+        self.col = pr.Color(60, 124, 58, 255) \
+            if (self.pos[0] + self.pos[1]*3 + self.pos[2] * 9) % 2 != 0 else pr.WHITE
+        self.cs = cs
+        self.size = size
 
     def draw(self):
-        pr.draw_cube_v(self.pos, [1,1,1], self.col)
+        pr.draw_cube_v([(n*3) - (self.size*self.cs)/2 + self.cs/2 for n in self.pos], self.dims, self.col)
 
 def main():
-    cube = [[[] for i in range(0,3)]for i in range(0,3)]
+    size = 8
+    cubelet_size = 3
 
-    for y in range(0, 3):
-        for z in range(0, 3):
-            for x in range(0, 3):
-                cube[y][z].append(Cubelet([(x-1) * 3,(y-1) * 3,(z-1) * 3]))
-                print((x-1) * 3,(y-1) * 3,(z-1) * 3)
-                
-    for y in range(0, 3):
-        for z in range(0, 3):
-            for x in range(0,3):
-                print(str(cube[y][z][x]), cube[y][z][x].pos)
+    cube = [[[] for i in range(0,size)]for i in range(0,size)]
 
+    for y in range(0, size):
+        for z in range(0, size):
+            for x in range(0, size):
+                cube[y][z].append(Cubelet([x,y,z], size, cubelet_size))
 
     pr.init_window(700, 700, "Rotochess")
     pr.set_target_fps(60)
@@ -36,13 +37,13 @@ def main():
     while not pr.window_should_close():
         pr.update_camera(camera)
         pr.begin_drawing()
-        pr.clear_background(pr.RAYWHITE)
+        pr.clear_background(pr.Color(182,191,239,255))
         pr.begin_mode_3d(camera)
         
-        for y in range(0, 3):
-            for z in range(0, 3):
-                for x in range(0, 3):
-                    pr.draw_cube_v(cube[y][z][x].pos, [1,1,1], pr.BLACK)
+        for y in range(0, size):
+            for z in range(0, size):
+                for x in range(0, size):
+                    cube[y][z][x].draw()
 
         pr.end_mode_3d()
         pr.end_drawing()
